d7723c7219d8b7828582e0b7ab82fe28c2d84e09
Takayuki Nakamura <nakamuraprocess@gmail.com>
2025-11-17 17:23:02 +0900

    Add: Time measure

--- pi4/speed_test_multi.py
+++ pi4/speed_test_multi.py
@@ -3,14 +3,14 @@ import threading
 import time
 
 # LED strip configuration:
-LED_MAX_SIZE      = 120      # Number of LED pixels.
-LED_PIN_1       = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-LED_PIN_2       = 21      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_MAX_SIZE   = 120     # Number of LED pixels.
+LED_PIN_1      = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN_2      = 21      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 65     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 65      # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0      # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 COLOR_BASE = Color(255, 255, 255)
 COLOR_NONE = Color(0, 0, 0)
@@ -18,9 +18,14 @@ COLOR_NONE = Color(0, 0, 0)
 thread_1_loop = True
 thread_2_loop = True
 
+time_start = 0.0
+time_finish = 0.0
+
 def color_wipe_forward(strip):
+    global time_start
     for i in range(LED_MAX_SIZE):
         if i == 0:
+            time_start = time.perf_counter()
             strip.setPixelColor(LED_MAX_SIZE-1, COLOR_NONE)
         else:
             strip.setPixelColor(i-1, COLOR_NONE)
@@ -28,8 +33,10 @@ def color_wipe_forward(strip):
         strip.show()
 
 def color_wipe_reverse(strip):
+    global time_finish
     for i in reversed(range(LED_MAX_SIZE)):
         if i == LED_MAX_SIZE-1:
+            time_finish = time.perf_counter()
             strip.setPixelColor(0, COLOR_NONE)
         else:
             strip.setPixelColor(i+1, COLOR_NONE)
@@ -42,14 +49,16 @@ def color_wipe_all(strip):
     strip.show()
 
 def thread_strip_1():
-    while thread_1_loop:
-        color_wipe_forward(strip_1)
-        color_wipe_reverse(strip_1)
+    global thread_1_loop
+    color_wipe_forward(strip_1)
+    color_wipe_reverse(strip_1)
+    thread_1_loop = False
 
 def thread_strip_2():
-    while thread_2_loop:
-        color_wipe_forward(strip_2)
-        color_wipe_reverse(strip_2)
+    global thread_2_loop
+    color_wipe_forward(strip_2)
+    color_wipe_reverse(strip_2)
+    thread_2_loop = False
 
 if __name__ == '__main__':
     strip_1 = Adafruit_NeoPixel(LED_MAX_SIZE, LED_PIN_1, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
@@ -66,15 +75,16 @@ if __name__ == '__main__':
     try:
         while True:
             time.sleep(1)
+            if thread_1_loop == True & thread_2_loop == True:
+                break
     
     except KeyboardInterrupt:
         print(">>> Ctrl + C detected! Stopping...")
 
     finally:
         print("Finaly")
-        thread_1_loop = False
-        thread_2_loop = False
         thread_1.join()
         thread_2.join()
         color_wipe_all(strip_1)
         color_wipe_all(strip_2)
+        print(f"TIME: {time_finish - time_start} sec")
--- pi4/speed_test_single.py
+++ pi4/speed_test_single.py
@@ -1,7 +1,8 @@
 from rpi_ws281x import *
+import time
 
 # LED strip configuration:
-LED_MAX_SIZE      = 120      # Number of LED pixels.
+LED_MAX_SIZE   = 120      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -13,9 +14,14 @@ LED_CHANNEL    = 0      # set to '1' for GPIOs 13, 19, 41, 45 or 53
 COLOR_BASE = Color(255, 255, 255)
 COLOR_NONE = Color(0, 0, 0)
 
+time_start = 0.0
+time_finish = 0.0
+
 def color_wipe_forward(strip):
+    global time_start
     for i in range(LED_MAX_SIZE):
         if i == 0:
+            time_start = time.perf_counter()
             strip.setPixelColor(LED_MAX_SIZE-1, COLOR_NONE)
         else:
             strip.setPixelColor(i-1, COLOR_NONE)
@@ -23,8 +29,10 @@ def color_wipe_forward(strip):
         strip.show()
 
 def color_wipe_reverse(strip):
+    global time_finish
     for i in reversed(range(LED_MAX_SIZE)):
         if i == LED_MAX_SIZE-1:
+            time_finish = time.perf_counter()
             strip.setPixelColor(0, COLOR_NONE)
         else:
             strip.setPixelColor(i+1, COLOR_NONE)
@@ -41,10 +49,10 @@ if __name__ == '__main__':
     strip.begin()
 
     try:
-        while True:
-            color_wipe_forward(strip)
-            color_wipe_reverse(strip)
+        color_wipe_forward(strip)
+        color_wipe_reverse(strip)
 
     finally:
         print("FINALLY")
         color_wipe_all(strip)
+        print(f"TIME: {time_finish - time_start} sec")
