eede09942490e6c29db1f329878727abf5d31773
Takayuki Nakamura <nakamuraprocess@gmail.com>
2025-11-17 18:34:59 +0900

    Change: britness = 255

--- pi4/speed_test_single.py
+++ pi4/speed_test_single.py
@@ -2,14 +2,14 @@ from rpi_ws281x import *
 import time
 
 # LED strip configuration:
-LED_MAX_SIZE   = 120      # Number of LED pixels.
+LED_MAX_SIZE   = 120     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+#LED_PIN        = 10     # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 65     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0      # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 COLOR_BASE = Color(255, 255, 255)
 COLOR_NONE = Color(0, 0, 0)
