1bdc90926fcf115f9263f46b7b09665a1fb9f1d5
Takayuki Nakamura <nakamuraprocess@gmail.com>
2025-11-21 16:28:59 +0900

    Add: Using multi thread in Teensy

--- teensy4.0/speed_test_multi/speed_test_multi.ino
+++ teensy4.0/speed_test_multi/speed_test_multi.ino
@@ -1,100 +1,90 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 #include <TeensyThreads.h>
 
 #define LED_MAX_SIZE 120
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
 
-Adafruit_NeoPixel strip1(LED_MAX_SIZE, 1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2(LED_MAX_SIZE, 6, NEO_GRB + NEO_KHZ800);
-
-uint32_t COLOR_BASE = strip1.Color(255, 255, 255);
-uint32_t COLOR_NONE = strip1.Color(0, 0, 0);
+CRGB strip1[LED_MAX_SIZE];
+CRGB strip2[LED_MAX_SIZE];
 
 bool bLoop1 = true;
 bool bLoop2 = true;
 unsigned long timeStart = 0;
 unsigned long timeFinish = 0;
 
-void colorWipeForward(Adafruit_NeoPixel &strip){
-  for(int i = 0; i < LED_MAX_SIZE; i++){
-    if(i == 0){
-      timeStart = millis();
-      strip.setPixelColor(LED_MAX_SIZE-1, COLOR_NONE);
+void tread1(){
+  if(bLoop1){
+    for (int i = 0; i < LED_MAX_SIZE; i++) {
+      if(i == 0){
+        timeStart = millis();
+        strip2[LED_MAX_SIZE-1] =  CRGB::Black;
+      }
+      else{
+        strip2[i-1] = CRGB::Black;
+      }
+      strip2[i] = CRGB::White;
+      FastLED.show();
+      //threads.delay(1);
     }
-    else{
-      strip.setPixelColor(i-1, COLOR_NONE);
+    for (int i = LED_MAX_SIZE - 1; i >= 0; i--) {
+      if(i == LED_MAX_SIZE-1){
+        timeFinish = millis();
+        strip2[0] = CRGB::Black;
+      }
+      else{
+        strip2[i+1] = CRGB::Black;
+      }
+      strip2[i] = CRGB::White;
+      FastLED.show();
+      //threads.delay(1);
     }
-    strip.setPixelColor(i, COLOR_BASE);
-    strip.show();
+    bLoop1 = false;
   }
 }
-
-void colorWipeReverse(Adafruit_NeoPixel &strip){
-  for(int i = LED_MAX_SIZE; i >= 0; i--){
-    if(i == LED_MAX_SIZE-1){
-      timeFinish = millis();
-      strip.setPixelColor(0, COLOR_NONE);
-    }
-    else{
-      strip.setPixelColor(i+1, COLOR_NONE);
-    }
-    strip.setPixelColor(i, COLOR_BASE);
-    strip.show();
-  }
-}
-
-void colorWipeAll(Adafruit_NeoPixel &strip){
-  for(int i = 0; i < LED_MAX_SIZE; i++){
-    strip.setPixelColor(i, COLOR_NONE);
-    strip.show();
-  }
-}
-
-void task1() {
-  while (true) {
-    if(bLoop1){
-      colorWipeForward(strip1);
-      colorWipeReverse(strip1);
-      bLoop1 = false;
-    }
-    else {
-      threads.delay(1);
-    }
-  }
-}
-
-void task2() {
-  while (true) {
-    if(bLoop2){
-      colorWipeForward(strip2);
-      colorWipeReverse(strip2);
-      bLoop2 = false;
-    }
-    else {
-      threads.delay(1);
-    }
-  }
-}
-
 
 void setup() {
-  Serial.begin(9600);
+  FastLED.addLeds<LED_TYPE, 1, GRB>(strip1, LED_MAX_SIZE);
+  FastLED.addLeds<LED_TYPE, 6, GRB>(strip2, LED_MAX_SIZE);
+  FastLED.setBrightness(255);
 
-  strip1.begin();
-  strip1.show();
-  strip1.setBrightness(255);
-
-  strip2.begin();
-  strip2.show();
-  strip2.setBrightness(255);
-
-  threads.addThread(task1);
-  threads.addThread(task2);
+  threads.addThread(tread1);
 }
 
 void loop() {
-  if(!bLoop1 && !bLoop2){
+  if(bLoop1){
+    for (int i = 0; i < LED_MAX_SIZE; i++) {
+      if(i == 0){
+        timeStart = millis();
+        strip1[LED_MAX_SIZE-1] =  CRGB::Black;
+      }
+      else{
+        strip1[i-1] = CRGB::Black;
+      }
+      strip1[i] = CRGB::White;
+      FastLED.show();
+      delay(1);
+    }
+    for (int i = LED_MAX_SIZE - 1; i >= 0; i--) {
+      if(i == LED_MAX_SIZE-1){
+        timeFinish = millis();
+        strip1[0] = CRGB::Black;
+      }
+      else{
+        strip1[i+1] = CRGB::Black;
+      }
+      strip1[i] = CRGB::White;
+      FastLED.show();
+      delay(1);
+    }
+    bLoop1 = false;
+  }
+  else {
+    FastLED.clear();
+    FastLED.show();
+    delay(1);
+  }
+  if(timeFinish > 0.0 && timeStart > 0.0){
     Serial.println(String("TIME: ") + float(timeFinish - timeStart) / 1000 + String(" sec"));
-    colorWipeAll(strip1);
-    colorWipeAll(strip2);
   }
 }
--- teensy4.0/speed_test_single/speed_test_single.ino
+++ teensy4.0/speed_test_single/speed_test_single.ino
@@ -1,65 +1,55 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
 #define LED_PIN 6
 #define LED_MAX_SIZE 120
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
 
-Adafruit_NeoPixel strip(LED_MAX_SIZE, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-uint32_t COLOR_BASE = strip.Color(255, 255, 255);
-uint32_t COLOR_NONE = strip.Color(0, 0, 0);
+CRGB leds[LED_MAX_SIZE];
 
 bool bLoop = true;
 unsigned long timeStart = 0;
 unsigned long timeFinish = 0;
 
-void colorWipeForward(Adafruit_NeoPixel &strip){
-  for(int i = 0; i < LED_MAX_SIZE; i++){
-    if(i == 0){
-      timeStart = millis();
-      strip.setPixelColor(LED_MAX_SIZE-1, COLOR_NONE);
-    }
-    else{
-      strip.setPixelColor(i-1, COLOR_NONE);
-    }
-    strip.setPixelColor(i, COLOR_BASE);
-    strip.show();
-  }
-}
-
-void colorWipeReverse(Adafruit_NeoPixel &strip){
-  for(int i = LED_MAX_SIZE; i >= 0; i--){
-    if(i == LED_MAX_SIZE-1){
-      timeFinish = millis();
-      strip.setPixelColor(0, COLOR_NONE);
-    }
-    else{
-      strip.setPixelColor(i+1, COLOR_NONE);
-    }
-    strip.setPixelColor(i, COLOR_BASE);
-    strip.show();
-  }
-}
-
-void colorWipeAll(Adafruit_NeoPixel &strip){
-  for(int i = 0; i < LED_MAX_SIZE; i++){
-    strip.setPixelColor(i, COLOR_NONE);
-    strip.show();
-  }
-}
-
 void setup() {
-  Serial.begin(9600);
-  strip.begin();
-  strip.show();
-  strip.setBrightness(255);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, LED_MAX_SIZE);
+  FastLED.setBrightness(255);
 }
 
 void loop() {
   if(bLoop){
-    colorWipeForward(strip);
-    colorWipeReverse(strip);
-    colorWipeAll(strip);
+    for (int i = 0; i < LED_MAX_SIZE; i++) {
+      if(i == 0){
+        timeStart = millis();
+        leds[LED_MAX_SIZE-1] =  CRGB::Black;
+      }
+      else{
+        leds[i-1] =  CRGB::Black;
+      }
+      leds[i] = CRGB::White;
+      FastLED.show();
+      delay(1);
+    }
+    for (int i = LED_MAX_SIZE - 1; i >= 0; i--) {
+      if(i == LED_MAX_SIZE-1){
+        timeFinish = millis();
+        leds[0] = CRGB::Black;
+      }
+      else{
+        leds[i+1] = CRGB::Black;
+      }
+      leds[i] = CRGB::White;
+      FastLED.show();
+      delay(1);
+    }
     bLoop = false;
+  }
+  else {
+    for (int i = 0; i < LED_MAX_SIZE; i++) {
+      leds[i] = CRGB::Black;
+      FastLED.show();
+      delay(1);
+    }
   }
   if(timeFinish > 0.0 && timeStart > 0.0){
     Serial.println(String("TIME: ") + float(timeFinish - timeStart) / 1000 + String(" sec"));
